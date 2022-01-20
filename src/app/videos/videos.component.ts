7e629e97c3c1d9be0f14fa243aba6c9a47eae226
unknown <RYZEN7@SISTEMAS-WEB.aguainmaculada.local>
2022-01-20 11:32:45 -0600

    Actualizacion de las cards

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -10,6 +10,7 @@ export class VideosComponent implements OnInit {
 
   @ViewChild('videoPlay', { static: false }) videoPlay: ElementRef;
   @ViewChild('modalMensaje', { static: false })modalMensaje:ElementRef;
+  @ViewChild('playCardNetflix', { static: false})playCardNetflix:ElementRef;
   
   public videoSeleccionado:number;
 
@@ -20,7 +21,7 @@ export class VideosComponent implements OnInit {
   }
 
   over(id:number): void {
-    this.videoSeleccionado=id;
+    //this.videoSeleccionado=id;
     const video=this.videoPlay.nativeElement;
 
     console.log("Hover activado", id); 
@@ -28,18 +29,32 @@ export class VideosComponent implements OnInit {
     video.play(); 
 
   }
-
   out() {
     const video=this.videoPlay.nativeElement;
     console.log("toy juera")
     video.pause();
+  }
+
+  
+  overNetflix(){
+    const video=this.playCardNetflix.nativeElement;
+    video.muted=true;
+    video.play();
+}
+
+  outCardVideo(){
+    const video=this.playCardNetflix.nativeElement;
+    video.pause();
 
   }
 
+
+  //Modal de video 
   public playVideo(){
  
     this.modalService.open(this.modalMensaje);
     
     console.log("Funciono")
   }
+
 }
