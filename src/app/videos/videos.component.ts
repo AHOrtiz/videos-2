455e4dd099232deee567f27fa6c3b99c1c8d928b
unknown <RYZEN7@SISTEMAS-WEB.aguainmaculada.local>
2022-01-20 16:58:54 -0600

    Reproduccion de video de cada card

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,44 +1,63 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
-  styleUrls: ['./videos.component.css']
+  styleUrls: ['./videos.component.css'],
 })
 
-export class VideosComponent implements OnInit {
-
-  @ViewChild('modalMensaje', { static: false })modalMensaje:ElementRef;
-  @ViewChild('playCardNetflix', { static: false})playCardNetflix:ElementRef;
-  
-  public videoSeleccionado:number;
-
-
+// Aqui se agrego el ciclo del componente (AfterViewInit) el cual se ejecuta cuando termiana de pintar el HTML
+export class VideosComponent implements AfterViewInit {
   constructor(private modalService: NgbModal) { }
 
-  ngOnInit(): void {
+
+  @ViewChild('modalMensaje', { static: false }) modalMensaje: ElementRef;
+  //  Aqui se declaro la obtencion del contenedor del HTML
+  @ViewChild('contenedorVideos', { static: false }) contenedorVideos: ElementRef<HTMLElement>;  
+  
+  // TODO: Esta variable es para tener las cards del HTML
+  private listCardsHTML: NodeListOf<ChildNode>
+  // TODO: Esta variable es para tener el video actual
+  private currentVideo: HTMLVideoElement
+
+  /**
+   * Se manda a llamar cuando se finaliza de pintar el HTML (en este caso todos los videos)
+   */
+  ngAfterViewInit(): void {
+    this.listCardsHTML = this.contenedorVideos.nativeElement.childNodes
+    console.log("Lista de Cards:", this.listCardsHTML);
   }
 
- 
-  //Evento para reproducir el video cuando se pasa el cursor
-  overNetflix(){
-    const video=this.playCardNetflix.nativeElement;
-    video.muted=true;
-    video.play();
-}
+  
+  public overNetflix(idVideo: number) {
+    console.log("mouseover en video: ", idVideo);
 
-  outCardVideo(){
-    const video=this.playCardNetflix.nativeElement;
-    video.pause();
+    // Se obtienen el nodo de video y se parsea a HTMLVideoElement para poder controlar sus eventos
+    const videoHTML: HTMLVideoElement = this.listCardsHTML[idVideo].childNodes[0] as HTMLVideoElement
+    console.log("Nodo", videoHTML);
 
+    // TODO: Se inicializa la variable para tener el video actual (en este caso esta variable se ocupa cuando se hace el outCardVideo)
+    this.currentVideo = videoHTML
+    this.currentVideo.muted = true;
+    this.currentVideo.play();
   }
-
-  //Modal de video 
-  public playVideo(){
- 
+  /**
+   * Detectar cuando cursor sale de una card de tipo netflix
+   */
+  public outCardVideo() {
+    this.currentVideo.pause()
+  }
+  /**
+   * Abrir una modal cuando seleccionan el icono de reproducir
+   */
+  public playVideo() {
     this.modalService.open(this.modalMensaje);
-    
-    console.log("Funciono")
+    console.log('Funciono');
   }
-
 }
